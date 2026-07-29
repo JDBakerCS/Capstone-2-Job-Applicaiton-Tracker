a9b8ad43f2d9dead500344514f52ce4bcf51de94
James Dalton Baker <james.Baker59@stu.bmcc.cuny.edu>
2026-07-29 15:47:24 -0400

    added imports and Route in App.jsx for DetailsPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,18 @@
+import { Routes, Route } from "react-router-dom";
 import ApplicationPage from "./pages/ApplicationPage";
+import ApplicationDetailsPage from "./pages/ApplicationDetailsPage"
 import "./App.css";
 
 function App() {
-  return <ApplicationPage />;
+  return (
+    <Routes>
+      <Route path="/" element={<ApplicationPage/>}/>
+      <Route
+        path="/applications/:id"
+        element={<ApplicationDetailsPage/>}
+        />
+    </Routes>
+  )
 }
 
 export default App;
